94a2928f8bfe9256bc57a02d9e1642b54f548b77
ohlseng01 <ohlseng01@student.psdr3.org>
2020-07-14 00:03:20 -0500

    Initial Commit

---
+++ partnerPartyGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  partnerPartyGenerator
+//
+//  Created by Grace Ohlsen on 7/14/20.
+//  Copyright © 2020 Grace Ohlsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
